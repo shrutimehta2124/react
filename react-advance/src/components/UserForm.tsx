72aed4fabf008561fcf7f0f3de47b3188ac9fbb7
shrutimehta2124 <shrutimehta.sarvadhi@gmail.com>
2025-03-10 16:39:29 +0530

    added

--- react-advance/src/components/UserForm.tsx
+++ react-advance/src/components/UserForm.tsx
@@ -1,36 +1,61 @@
+import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useAppDispatch } from '../store/hooks';
-import { addUser } from '../store/slices/userSlice';
 
-interface UserFormData {
-  name: string;
+interface FormData {
+  username: string;
   email: string;
 }
 
-const UserForm = () => {
-  const dispatch = useAppDispatch();
-  const { register, handleSubmit, reset } = useForm<UserFormData>();
+const UserForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const onSubmit = (data: UserFormData) => {
-    dispatch(addUser(data));
-    reset(); // Reset form after submission
+  const onSubmit = (data: FormData) => {
+    // Call API to save data
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(() => alert('User added successfully!'));
   };
 
   return (
-    <div>
-      <h2>User Form</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Name:</label>
-        <input {...register('name', { required: true })} />
-        <br />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* Username Field */}
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          {...register('username', { required: 'Username is required' })}
+        />
+        {errors.username && (
+          <p style={{ color: 'red' }}>{errors.username.message}</p>
+        )}
+      </div>
 
+      {/* Email Field */}
+      <div>
         <label>Email:</label>
-        <input {...register('email', { required: true })} />
-        <br />
+        <input
+          type="email"
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              message: 'Invalid email format',
+            },
+          })}
+        />
+        {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+      </div>
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
